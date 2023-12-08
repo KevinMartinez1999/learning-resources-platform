2fd13f2b1e4558956bdf0b4abda13f4b90725a41
KevinMartinez1999 <kevin.martinez1@udea.edu.co>
2023-12-08 07:40:04 -0500

    Refactor resource form and dashboard layout

--- resources/js/Components/ResourceForm.vue
+++ resources/js/Components/ResourceForm.vue
@@ -31,16 +31,29 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="m-6">
-        <input type="text" v-model="title">
-        <input type="text" v-model="description">
-        <input type="text" v-model="link">
-        <select v-model="category_id">
-            <option v-for="category in categories" :key="category.id" :value="category.id">
-                {{ category.name }}
-            </option>
-        </select>
-        <button @click="createResource" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+    <div class="w-full flex flex-col m-6">
+        <div class="mb-4">
+            <p>Título:</p>
+            <input type="text" v-model="title" class="w-1/3 rounded">
+        </div>
+        <div class="mt-4 mb-4">
+            <p>Categoria:</p>
+            <select v-model="category_id" class="w-1/3 rounded">
+                <option v-for="category in categories" :key="category.id" :value="category.id">
+                    {{ category.name }}
+                </option>
+            </select>
+        </div>
+        <div class="mt-4 mb-4">
+            <p>Link:</p>
+            <input type="text" v-model="link" class="w-1/3 rounded">
+        </div>
+        <div class="mt-4 mb-4">
+            <p>Descripción:</p>
+            <textarea v-model="description" class="w-1/2 h-48 rounded"></textarea>
+        </div>
+        <button @click="createResource"
+            class="w-28 py-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-700 duration-300 font-bold text-white rounded">
             Crear recurso
         </button>
     </div>
